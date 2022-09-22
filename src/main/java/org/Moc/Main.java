54e4ede8b6e9a8f1e838e1af0c7211eca42af170
moc <1287607885@qq.com>
2022-09-22 13:25:51 +0800

    .

---
+++ src/main/java/org/Moc/Main.java
@@ -0,0 +1,12 @@
+package org.Moc;
+/**
+ * @author moc
+ * @date 2022-09-22 13:25
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
