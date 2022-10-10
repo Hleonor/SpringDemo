947900f0e63afba7b2189b22a4a08e8b80e6d61b
moc <1287607885@qq.com>
2022-10-10 12:10:12 +0800

    通过注解方式的include和exclude实现配置细节

--- src/main/java/com/springdemo02/service/UserService.java
+++ src/main/java/com/springdemo02/service/UserService.java
@@ -1,10 +1,9 @@
 package com.springdemo02.service;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 // 在注解里面value属性值可以省略不写，默认值是类名首字母小写
-@Service(value = "userService") // 等价于 <bean id = "Sekiro" class="类的路径"
+@Service(value = "userService") // 等价于 <bean id = "Sekiro" class="类的路径">
 public class UserService
 {
     public void add()
