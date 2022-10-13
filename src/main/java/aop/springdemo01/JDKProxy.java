bd9d6b7424ccd0312ab0c5f0c4b69b80e1033c41
moc <1287607885@qq.com>
2022-10-13 10:56:42 +0800

    AOP初识增加自己对代码的理解

--- src/main/java/aop/springdemo01/JDKProxy.java
+++ src/main/java/aop/springdemo01/JDKProxy.java
@@ -11,7 +11,7 @@ public class JDKProxy
 {
     public static void main(String[] args)
     {
-        // 创建接口实现类代理对象
+        // 创建接口实现类代理对象，保存了一系列的具体实现类
         Class[] interfaces = {UserDao.class};
         /*Proxy.newProxyInstance(JDKProxy.class.getClassLoader(), interfaces, new InvocationHandler()
         {
@@ -21,6 +21,10 @@ public class JDKProxy
                 return null;
             }
         });*/
+        /**
+         * 当需要增强逻辑时，只需要在invoke方法中添加即可
+         * 当需要增加一个代理时，只需要在newProxyInstance方法中添加即可，并且需要实现InvocationHandler接口
+         */
         UserDaoImpl userDao = new UserDaoImpl();
         UserDao dao = (UserDao) Proxy.newProxyInstance(JDKProxy.class.getClassLoader(), interfaces, new UserDaoProxy(userDao));
         System.out.println("result = " + dao.add(1 , 2));
@@ -32,7 +36,7 @@ class UserDaoProxy implements InvocationHandler
 {
     // 1、明确代理的对象，将被代理的对象传入
     // 通过有参构造传递
-    public Object obj;
+    public Object obj; // 本例中传入的类是userDao
     public UserDaoProxy(Object obj)
     {
         this.obj = obj;
--- src/main/java/aop/springdemo01/UserDao.java
+++ src/main/java/aop/springdemo01/UserDao.java
@@ -2,6 +2,7 @@ package aop.springdemo01;
 
 public interface UserDao
 {
+    // 两个待实现的方法
     int add(int a, int b);
 
     String update(String id);
