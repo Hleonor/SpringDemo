24c7c7e0bd4e521c5a10dd30b906d75c85867ec7
moc <1287607885@qq.com>
2022-10-18 16:06:55 +0800

    创建user类，与数据库交互

---
+++ src/main/java/jdbctemplate/entity/user.java
@@ -0,0 +1,38 @@
+package jdbctemplate.entity;
+
+public class user
+{
+    public String getUserId()
+    {
+        return userId;
+    }
+
+    public void setUserId(String userId)
+    {
+        this.userId = userId;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+
+    public String getuStatus()
+    {
+        return uStatus;
+    }
+
+    public void setuStatus(String uStatus)
+    {
+        this.uStatus = uStatus;
+    }
+
+    private String userId;
+    private String userName;
+    private String uStatus;
+}
