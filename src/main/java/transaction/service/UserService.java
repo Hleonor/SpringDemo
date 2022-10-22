738aff4c29f2c08b391867d6235527f448e02152
moc <1287607885@qq.com>
2022-10-22 15:45:54 +0800

    通过注解的方式完成事务的声明和配置

--- src/main/java/transaction/service/UserService.java
+++ src/main/java/transaction/service/UserService.java
@@ -2,9 +2,11 @@ package transaction.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import transaction.dao.UserDao;
 
 @Service
+@Transactional
 public class UserService
 {
     // 注入dao
@@ -14,10 +16,27 @@ public class UserService
     // 转账的方法
     public void accountMoney()
     {
-        // 只狼少100钱
-        userDao.reduceMoney();
+//        try
+//        {
+//            // 第一步开启事务操作
+//
+//            // 第二步；进行业务操作
+//
 
-        // 神子多100
-        userDao.addMoney();
+            // 只狼少100钱
+            userDao.reduceMoney();
+
+             // 模拟异常
+             int i = 10 / 0;
+
+            // 神子多100
+            userDao.addMoney();
+
+            // 第三步，没有异常，提交事务
+//        }
+//        catch (Exception ee)
+//        {
+//            // 第四步，捕获异常，事务rollback
+//        }
     }
 }
