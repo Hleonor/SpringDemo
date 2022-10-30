c28beef75fd677d0c31ca3e3fd527e937a99a823
moc <1287607885@qq.com>
2022-10-30 09:21:16 +0800

    函数式风格创建对象

--- src/main/java/transaction/test/TestTransfer.java
+++ src/main/java/transaction/test/TestTransfer.java
@@ -3,6 +3,7 @@ package transaction.test;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 import transaction.config.TxConfig;
 import transaction.service.UserService;
 
@@ -33,4 +34,17 @@ public class TestTransfer
         userService.accountMoney();
     }
 
+    // 函数式风格创建对象，交给spring管理
+    @Test
+    public void testGenericApplication()
+    {
+        // 1.创建GenericApplicationContext对象
+        GenericApplicationContext context = new GenericApplicationContext();
+        // 2.调用context的方法对象注册
+        context.refresh();
+        context.registerBean("user1", User.class, () -> new User());
+        // 3.获取在spring注册的对象
+        User user = (User) context.getBean("user1");
+        System.out.println(user);
+    }
 }
---
+++ src/main/java/transaction/test/User.java
@@ -0,0 +1,5 @@
+package transaction.test;
+
+public class User
+{
+}
